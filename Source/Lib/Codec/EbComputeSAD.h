073d27e07964755294458958842487ec297b8930
Slawomir Pawlowski <slawomir.pawlowski@intel.com>
2019-02-14 13:03:17 +0100

    Convert Compute4xMSadSub_SSE2_INTRIN to AVX2

--- Source/Lib/Codec/EbComputeSAD.h
+++ Source/Lib/Codec/EbComputeSAD.h
@@ -97,7 +97,7 @@ extern "C" {
     {
         // NON_AVX2
         {
-            /*0 4xM  */ Compute4xMSadSub_SSE2_INTRIN,
+            /*0 4xM  */ Compute4xMSadSub_AVX2_INTRIN,
             /*1 8xM  */ FastLoop_NxMSadKernel,
             /*2 16xM */ FastLoop_NxMSadKernel,
             /*3 24xM */ FastLoop_NxMSadKernel,
@@ -110,7 +110,7 @@ extern "C" {
         },
         // AVX2
         {
-            /*0 4xM  */ Compute4xMSadSub_SSE2_INTRIN,
+            /*0 4xM  */ Compute4xMSadSub_AVX2_INTRIN,
             /*1 8xM  */ Compute8xMSad_AVX2_INTRIN,
             /*2 16xM */ Compute16xMSad_AVX2_INTRIN,
             /*3 24xM */ FastLoop_NxMSadKernel,
