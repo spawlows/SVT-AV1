73476e7fcf3c53d1419fb60d8553cceb6abc1ef7
hguermaz <hassen.guermazi@intel.com>
2019-03-07 22:10:50 -0800

    fixed full_loop_escape for M7

--- Source/Lib/Codec/EbEncDecProcess.c
+++ Source/Lib/Codec/EbEncDecProcess.c
@@ -1388,7 +1388,7 @@ EbErrorType signal_derivation_enc_dec_kernel_oq(
     // 0                    Off
     // 1                    On but only INTRA
     // 2                    On both INTRA and INTER
-    if (picture_control_set_ptr->enc_mode <= ENC_M6)
+    if (picture_control_set_ptr->enc_mode <= ENC_M7)
         context_ptr->full_loop_escape = 0;
     else
         context_ptr->full_loop_escape = 1;
